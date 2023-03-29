fdd09e83e61395136ef1fdc6929d1861ede7b684
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-03-29 15:49:01 +0900

    change function

---
+++ mandatory/threads/manage_threads.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   manage_threads.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/29 10:01:56 by minkim3           #+#    #+#             */
+/*   Updated: 2023/03/29 15:47:22 by minkim3          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../philo.h"
+
+int	start_threads(t_monitoring *monitoring, t_philo **philos)
+{
+	int	i;
+
+	i = -1;
+	while (++i < monitoring->number_of_philosophers)
+	{
+		if (pthread_create(&monitoring->threads[i], NULL, \
+			(void *)life_of_philo, (void *)(philos[i])) != 0)
+			return (print_error("Error: pthread_create() failed", monitoring));
+	}
+	i = -1;
+	while (++i < monitoring->number_of_philosophers)
+	{
+		if (pthread_join(monitoring->threads[i], NULL) != 0)
+			return (print_error("Error: pthread_join() failed", monitoring));
+	}
+	return (0);
+}
