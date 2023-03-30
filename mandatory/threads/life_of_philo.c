4358342c621b8792a5f3eb0d01f9cfb5e8edbf47
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-03-30 16:02:31 +0900

    Mutex lock before start

--- mandatory/threads/life_of_philo.c
+++ mandatory/threads/life_of_philo.c
@@ -6,7 +6,7 @@
 /*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 10:32:22 by minkim3           #+#    #+#             */
-/*   Updated: 2023/03/30 15:54:28 by minkim3          ###   ########.fr       */
+/*   Updated: 2023/03/30 16:02:00 by minkim3          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,11 @@ void	life_of_philo(void *arg)
 	t_monitoring	*monitoring;
 	t_philo			*philo;
 
+	
 	philo = (t_philo *)arg;
 	monitoring = philo->monitoring;
+	pthread_mutex_lock(monitoring->wait_before_start);
+	pthread_mutex_unlock(monitoring->wait_before_start);
 	if (philo->id % 2 == 0)
 		time_lapse(philo->time_to_eat / 2);
 	while (monitoring->all_live)
