607a19da3ba8e593a09fe5d5e165fd402b2a66b3
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-03-30 15:48:40 +0900

    make the time_lapse function

--- mandatory/threads/time_lapse.c
+++ mandatory/threads/time_lapse.c
@@ -6,12 +6,26 @@
 /*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 15:37:55 by minkim3           #+#    #+#             */
-/*   Updated: 2023/03/30 15:40:14 by minkim3          ###   ########.fr       */
+/*   Updated: 2023/03/30 15:48:18 by minkim3          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
 
-void	time_lapse(unsigend int time, t_philo *philo)
+void	time_lapse(long long time, t_philo *philo)
 {
+	struct timeval	start_time;
+	struct timeval	current_time;
+	long long		elapsed_time;
+
+	gettimeofday(&start_time, NULL);
+	while (1)
+	{
+		gettimeofday(&current_time, NULL);
+		elapsed_time = (current_time.tv_sec - start_time.tv_sec) * 1000LL +
+					   (current_time.tv_usec - start_time.tv_usec) / 1000LL;
+		if (elapsed_time >= time)
+			break;
+		usleep(1000);
+	}
 }
