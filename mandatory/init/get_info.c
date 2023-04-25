f8427933d28e5005aa5970e3b1a59d92fc8e88b1
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-04-25 21:09:47 +0900

    is integer

--- mandatory/init/get_info.c
+++ mandatory/init/get_info.c
@@ -6,20 +6,39 @@
 /*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 17:18:02 by minkim3           #+#    #+#             */
-/*   Updated: 2023/04/25 21:03:26 by minkim3          ###   ########.fr       */
+/*   Updated: 2023/04/25 21:09:19 by minkim3          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-/*
-** if is_integer() == true, ft_atoi. 
-*/
+int	is_integer(const char *str)
+{
+	if (str == NULL || *str == '\0')
+		return (0);
+	if (*str == '-')
+		return (0);
+	while (*str != '\0')
+	{
+		if (!('0' <= *str && *str <= '9'))
+			return (0);
+		str++;
+	}
+	return (1);
+}
+
 int	get_info(int argc, char *argv[], t_info *info)
 {
 	if (argc < 5 || argc > 6)
 	{
 		printf("Error: Invalid number of arguments.\n");
+		return (ERROR);
+	}
+	if (!is_integer(argv[1]) || !is_integer(argv[2]) ||
+		!is_integer(argv[3]) || !is_integer(argv[4]) ||
+		(argc == 6 && !is_integer(argv[5])))
+	{
+		printf("Error: Invalid argument values.\n");
 		return (ERROR);
 	}
 	info->number_of_philosophers = ft_atoi(argv[1]);
@@ -30,12 +49,5 @@ int	get_info(int argc, char *argv[], t_info *info)
 		info->must_eat_times = ft_atoi(argv[5]);
 	else
 		info->must_eat_times = -1;
-	if (info->number_of_philosophers <= 0 || info->time_to_die <= 0 || \
-		info->time_to_eat <= 0 || info->time_to_sleep <= 0 || \
-		(argc == 6 && info->must_eat_times < 0))
-	{
-		printf("Error: Invalid argument values.\n");
-		return (ERROR);
-	}
 	return (0);
 }
