fc0aaeeb7cfa79c4c9b5e64728fb24ae4182602f
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-04-25 21:40:03 +0900

    philo

--- mandatory/philo/print_state.c
+++ mandatory/philo/print_state.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print.c                                            :+:      :+:    :+:   */
+/*   print_state.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 19:11:32 by minkim3           #+#    #+#             */
-/*   Updated: 2023/04/25 21:21:16 by minkim3          ###   ########.fr       */
+/*   Updated: 2023/04/25 21:39:52 by minkim3          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,15 @@ int	print_state(t_philo *philo, const char *statement)
 {
 	unsigned int philo_time;
 
+	pthread_mutex_lock(&philo->resources->print);
 	pthread_mutex_lock(&(philo->resources->stop));
 	if (philo->resources->stop_all == TRUE)
 	{
 		pthread_mutex_unlock(&(philo->resources->stop));
+		pthread_mutex_unlock(&philo->resources->print);
 		return (ERROR);
 	}
 	pthread_mutex_unlock(&(philo->resources->stop));
-	pthread_mutex_lock(&philo->resources->print);
 	philo_time = get_time() - philo->info.start_time;
 	if (ft_strcmp(statement, "died") == 0)
 		printf(RED "%u %d %s" DEFAULT "\n", philo_time, philo->id, statement);
