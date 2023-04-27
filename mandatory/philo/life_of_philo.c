c3b1a9920cc6f5eb15929526b6683df478773747
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-04-27 12:31:09 +0900

    little inprove

--- mandatory/philo/life_of_philo.c
+++ mandatory/philo/life_of_philo.c
@@ -6,7 +6,7 @@
 /*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 18:58:36 by minkim3           #+#    #+#             */
-/*   Updated: 2023/04/26 19:38:40 by minkim3          ###   ########.fr       */
+/*   Updated: 2023/04/27 12:30:45 by minkim3          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,28 @@
 
 static void sleeping(t_philo *philo)
 {
-	give_usleep(philo->id);
     print_state(philo, "is sleeping");
     time_lapse(philo->info.time_to_sleep);
 }
 
 static void thinking(t_philo *philo)
 {
-	give_usleep(philo->id);
     print_state(philo, "is thinking");
 }
 
 void	give_usleep(int num)
 {
-	if (num < 20)
+	if (num > 1 && num < 5)
+		usleep(1);
+	else if (num < 20)
 		usleep(10);
-	else if (num < 25)
-		usleep(num);
 	else if (num < 30)
-		usleep(num * 2);
-	else if (num < 40)
-		usleep(num * 2.5);
-	else if (num < 50)
-		usleep(num * 3);
-	else if (num < 60)
-		usleep(num * 3.5);
+		usleep(num);
+	else if (num < 100)
+		usleep(num * (num / 10));
+	else
+		usleep(num * (num / 10) + num * (num / 100));
+
 }
 
 void	*life_of_philo(void *arg)
